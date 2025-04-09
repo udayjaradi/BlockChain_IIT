028dc828120a0e8c16f7360a100e009448794866
Uday <udayj@intellihinc.com>
2025-04-09 13:18:20 +0530

    new assignment upload

---
+++ VariableModify/abi.js
@@ -0,0 +1,59 @@
+const contractABI = [
+    [
+        {
+            "inputs": [],
+            "name": "decrementnumber",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "getnumber",
+            "outputs": [
+                {
+                    "internalType": "uint16",
+                    "name": "",
+                    "type": "uint16"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "incrementnumber",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "number",
+            "outputs": [
+                {
+                    "internalType": "uint16",
+                    "name": "",
+                    "type": "uint16"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "uint16",
+                    "name": "value",
+                    "type": "uint16"
+                }
+            ],
+            "name": "setnumber",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        }
+    ]
+  ];
+  const contractAddress = "0x3c725134d74D5c45B4E4ABd2e5e2a109b5541288"; // Replace with your address
+ //
